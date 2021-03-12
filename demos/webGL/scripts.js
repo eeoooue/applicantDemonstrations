9f004480d88154e12d8a47aa26eb0552c3874cac
Simon Grey <S.Grey@hull.ac.uk>
2021-03-12 15:27:28 +0000

    Added camera control tutorial starter

--- demos/webGL/scripts.js
+++ demos/webGL/scripts.js
@@ -1,4 +1,11 @@
-function writeHeader()
+var gl;
+var verticesString;
+var indicesString;
+var vertexShaderCode;
+var fragmentShaderCode;
+var cameraPosition;
+
+function writeHeader(callback)
 {
 	document.write("<div id=\"navBar\">\
 	<ul>\
@@ -10,41 +17,14 @@ function writeHeader()
 	</div>\
 	<div id=\"content\">\
       <canvas width = \"400\" height = \"400\" id = \"webGLCanvas\"></canvas>");
-		var data = "0.0, 0.75 ,0.0, 1.0, 0.0, 0.0,\
-            -0.75, -0.75, 0.0, 0.0, 1.0, 0.0,\
-            0.75, -0.75, 0.0, 0.0, 0.0, 1.0"
-		writeForm("reloadBuffers", data);
+		writeForm(callback);
 }
 
-function writeForm(callback, data)
+function initialiseVBOTutorial()
 {
-	document.write("<form action=\"javascript:" + callback + "()\">\
-		<textarea id=\"code\" name=\"vs\" rows=\"5\" cols=\"50\">"+ data + "</textarea>\
-		  <input type=\"submit\" value=\"Update\">\
-		</form>");
-}
-
-var gl;
-var verticesString = "0.0,0.75,0.0, 1.0, 0.0, 0.0,\
-            -0.75,-0.75,0.0, 0.0, 1.0, 0.0,\
-            0.75,-0.75,0.0, 0.0, 0.0, 1.0";
-var indicesString = "0,1,2";
-
-function initialiseWebGL()
-{
-      canvas = document.getElementById("webGLCanvas");
-      gl = canvas.getContext("webgl");
-	  
-		loadBuffers();
-         
-         /*=========Drawing the triangle===========*/
-
-         gl.clearColor(0.2, 0.2, 0.2, 1.0);
-         gl.enable(gl.DEPTH_TEST);
-         gl.clear(gl.COLOR_BUFFER_BIT);
-         gl.viewport(0,0,canvas.width,canvas.height);	  
-	  
-         var vertCode =
+	verticesString = "0.0, 0.75, 0.0, 1.0, 0.0, 0.0,-0.75, -0.75, 0.0, 0.0, 1.0, 0.0, 0.75, -0.75, 0.0, 0.0, 0.0, 1.0"
+	indicesString = "0,1,2";
+	vertexShaderCode =
             'attribute vec3 a_position;' +
 			'attribute vec3 a_colour;' +
 			'varying vec4 v_colour;' +
@@ -52,26 +32,62 @@ function initialiseWebGL()
                ' gl_Position = vec4(a_position, 1.0);' +
 			   ' v_colour = vec4(a_colour, 1.0);' +
             '}';
-            
-         var vertShader = gl.createShader(gl.VERTEX_SHADER);
-         gl.shaderSource(vertShader, vertCode);
-         gl.compileShader(vertShader);
-         var fragCode =
+	fragmentShaderCode = 
 			'precision mediump float;' +
 			'varying vec4 v_colour;' +
 			'void main(void) {' +
                ' gl_FragColor = v_colour;' +
+            '}';	
+	document.getElementById("code").value = verticesString;
+	initialiseWebGL();
+}
+
+function initialiseCameraTutorial()
+{
+	verticesString = "0.0, 0.75, 0.0, 1.0, 0.0, 0.0,-0.75, -0.75, 0.0, 0.0, 1.0, 0.0, 0.75, -0.75, 0.0, 0.0, 0.0, 1.0"
+	indicesString = "0,1,2";
+	cameraPosition = [ 0.0, 0.0, 0.0]; 
+	vertexShaderCode =
+            'attribute vec3 a_position;\r\n' +
+			'attribute vec3 a_colour;\r\n\r\n' +
+			'uniform vec3 u_cameraPosition;\r\n\r\n' +
+			'varying vec4 v_colour;\r\n\r\n' +
+            'void main(void) {\r\n' +
+               ' gl_Position = vec4(a_position, 1.0);\r\n' +
+			   ' v_colour = vec4(a_colour, 1.0);\r\n' +
             '}';
-         var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-         gl.shaderSource(fragShader, fragCode); 
-         gl.compileShader(fragShader);
-         shaderProgram = gl.createProgram();
-         gl.attachShader(shaderProgram, vertShader);
-         gl.attachShader(shaderProgram, fragShader);
-         gl.linkProgram(shaderProgram);
-         gl.useProgram(shaderProgram);	  
-         bindShaders();
-		render();
+	document.getElementById("code").value = vertexShaderCode;
+	fragmentShaderCode = 
+			'precision mediump float;' +
+			'varying vec4 v_colour;' +
+			'void main(void) {' +
+               ' gl_FragColor = v_colour;' +
+            '}';	
+	document.getElementById("code").value = vertexShaderCode;
+	initialiseWebGL();
+	document.addEventListener('keyup', onKeyUp, false);	
+}
+
+function writeForm(callback)
+{
+	document.write("<form action=\"javascript:" + callback + "()\">\
+		<textarea id=\"code\" name=\"vs\" rows=\"15\" cols=\"50\"></textarea>\
+		  <input type=\"submit\" value=\"Update\">\
+		</form>");
+}
+
+function initialiseWebGL()
+{
+    canvas = document.getElementById("webGLCanvas");
+    gl = canvas.getContext("webgl");
+    gl.clearColor(0.2, 0.2, 0.2, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(0,0,canvas.width,canvas.height);	  
+	loadBuffers();	  
+	loadShaders();
+    bindShaders();
+	render();
 }
 
 function render()
@@ -122,6 +138,81 @@ function reloadBuffers()
 		loadBuffers();
 		bindShaders();
 		render();
+}
+
+function loadShaders()
+{  
+         var vertShader = gl.createShader(gl.VERTEX_SHADER);
+         gl.shaderSource(vertShader, vertexShaderCode);
+         gl.compileShader(vertShader);
+         var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+         gl.shaderSource(fragShader, fragmentShaderCode);
+         gl.compileShader(fragShader);
+         shaderProgram = gl.createProgram();
+         gl.attachShader(shaderProgram, vertShader);
+         gl.attachShader(shaderProgram, fragShader);
+         gl.linkProgram(shaderProgram);
+         gl.useProgram(shaderProgram);		
+}
+
+function reloadVertexShader()
+{
+	vertexShaderCode = document.getElementById("code").value;
+	var vertShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertShader, vertexShaderCode);
+    gl.compileShader(vertShader);
+
+	var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragShader, fragmentShaderCode); 
+	gl.compileShader(fragShader);
+	shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertShader);
+	gl.attachShader(shaderProgram, fragShader);
+	gl.linkProgram(shaderProgram);
+	gl.useProgram(shaderProgram);
+	bindShaders();
+	var uCamPosLocation = gl.getUniformLocation(shaderProgram, "u_cameraPosition");
+	gl.uniform3f(uCamPosLocation,
+	cameraPosition[0], 
+	cameraPosition[1], 
+	cameraPosition[2], 
+	cameraPosition[3]);
+
+	render();
+}
+
+function updateCameraPosition()
+{
+	var uCamPosLocation = gl.getUniformLocation(shaderProgram, "u_cameraPosition");
+	gl.uniform3f(uCamPosLocation,
+		cameraPosition[0], 
+		cameraPosition[1], 
+		cameraPosition[2], 
+		cameraPosition[3]);
+}
+
+function onKeyUp(event)
+{
+	if (event.key == 'd')
+	{
+		cameraPosition[0] = cameraPosition[0] + 0.05;
+		updateCameraPosition();
+	}
+	else if (event.key == 'a')
+	{
+		cameraPosition[0] = cameraPosition[0] - 0.05;
+		updateCameraPosition();	}
+	else if (event.key == 'w')
+	{
+		cameraPosition[1] = cameraPosition[1] + 0.05;
+		updateCameraPosition();	}	
+	else if (event.key == 's')
+	{
+		cameraPosition[1] = cameraPosition[1] - 0.05;
+		updateCameraPosition();	
+	}
+	
+	render();
 }
 
 function writeFooter()
