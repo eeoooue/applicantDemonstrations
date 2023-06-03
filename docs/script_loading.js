b531c7dd9009df8a392449a933b17b76fe823acf
eeoooue <pdmoorhouse@gmail.com>
2023-06-03 09:00:48 +0100

    made a LoadingDemo class

--- docs/script_loading.js
+++ docs/script_loading.js
@@ -1,7 +1,31 @@
+import { WebGlHost } from "./webglhost.js";
 import { TriangleModel } from "./models/triangle.js";
-import { PageLoader } from "./pageloader.js";
-function LoadLoadingPage() {
-    const triangle = new TriangleModel();
-    PageLoader.initialiseVBOTutorial(triangle.vertices, triangle.indices);
+export class LoadingDemo {
+    constructor() {
+        this.vertexShaderCode = 'attribute vec3 a_position;' +
+            'attribute vec3 a_colour;' +
+            'varying vec4 v_colour;' +
+            'void main(void) {' +
+            ' gl_Position = vec4(a_position, 1.0);' +
+            ' v_colour = vec4(a_colour, 1.0);' +
+            '}';
+        this.fragmentShaderCode = 'precision mediump float;' +
+            'varying vec4 v_colour;' +
+            'void main(void) {' +
+            ' gl_FragColor = v_colour;' +
+            '}';
+        this.startingCode = "0.0, 0.75, 0.0, 1.0, 0.0, 0.0,-0.75, -0.75, 0.0, 0.0, 1.0, 0.0, 0.75, -0.75, 0.0, 0.0, 0.0, 1.0";
+        this.model = new TriangleModel();
+        this.initializeDemo();
+    }
+    initializeDemo() {
+        const codeSection = document.getElementById("code");
+        const startingCode = "0.0, 0.75, 0.0, 1.0, 0.0, 0.0,-0.75, -0.75, 0.0, 0.0, 1.0, 0.0, 0.75, -0.75, 0.0, 0.0, 0.0, 1.0";
+        if (codeSection && codeSection instanceof HTMLTextAreaElement) {
+            codeSection.value = startingCode;
+            var host = new WebGlHost(this.model.vertices, this.model.indices, this.vertexShaderCode, this.fragmentShaderCode, "loading");
+            host.loadingPageBindShaders();
+        }
+    }
 }
-LoadLoadingPage();
+new LoadingDemo();
--- src/script_loading.ts
+++ src/script_loading.ts
@@ -1,11 +1,48 @@
 
+
+import { WebGlHost } from "./webglhost.js";
 import { TriangleModel } from "./models/triangle.js";
-import { PageLoader } from "./pageloader.js";
 
-function LoadLoadingPage(): void  {
+export class LoadingDemo {
 
-    const triangle: TriangleModel = new TriangleModel();
-    PageLoader.initialiseVBOTutorial(triangle.vertices, triangle.indices);
+    public vertexShaderCode: string =
+        'attribute vec3 a_position;' +
+        'attribute vec3 a_colour;' +
+        'varying vec4 v_colour;' +
+        'void main(void) {' +
+        ' gl_Position = vec4(a_position, 1.0);' +
+        ' v_colour = vec4(a_colour, 1.0);' +
+        '}';
+
+    public fragmentShaderCode: string =
+        'precision mediump float;' +
+        'varying vec4 v_colour;' +
+        'void main(void) {' +
+        ' gl_FragColor = v_colour;' +
+        '}';
+
+    public startingCode: string = "0.0, 0.75, 0.0, 1.0, 0.0, 0.0,-0.75, -0.75, 0.0, 0.0, 1.0, 0.0, 0.75, -0.75, 0.0, 0.0, 0.0, 1.0";
+
+    public model: TriangleModel = new TriangleModel();
+
+    constructor() {
+
+        this.initializeDemo();
+    }
+
+    public initializeDemo() {
+
+        const codeSection: HTMLElement | null = document.getElementById("code");
+        const startingCode: string = "0.0, 0.75, 0.0, 1.0, 0.0, 0.0,-0.75, -0.75, 0.0, 0.0, 1.0, 0.0, 0.75, -0.75, 0.0, 0.0, 0.0, 1.0";
+
+        if (codeSection && codeSection instanceof HTMLTextAreaElement) {
+
+            codeSection.value = startingCode;
+            var host = new WebGlHost(this.model.vertices, this.model.indices, this.vertexShaderCode, this.fragmentShaderCode, "loading");
+            host.loadingPageBindShaders();
+        }
+    }
 }
 
-LoadLoadingPage();
+
+new LoadingDemo();
