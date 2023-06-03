e759abd7bb034eddc1485b71a078ffe005ca6148
eeoooue <pdmoorhouse@gmail.com>
2023-06-03 12:05:49 +0100

    removed camera position from starting textarea

--- src/script_camera.ts
+++ src/script_camera.ts
@@ -10,7 +10,7 @@ export class CameraDemo {
         'uniform vec3 u_cameraPosition;\r\n\r\n' +
         'varying vec4 v_colour;\r\n\r\n' +
         'void main(void) {\r\n' +
-        ' gl_Position = vec4(a_position - u_cameraPosition, 1.0);\r\n' +
+        ' gl_Position = vec4(a_position, 1.0);\r\n' +
         ' v_colour = vec4(a_normal * 0.5 + 0.5, 1.0);\r\n' +
         '}';
 
