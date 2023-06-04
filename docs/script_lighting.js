0445dd6ee0e73a7719c3a2bd8a72f73a4e964478
eeoooue <pdmoorhouse@gmail.com>
2023-06-04 10:06:53 +0100

    lighting page uses CodeParcel

--- docs/script_lighting.js
+++ docs/script_lighting.js
@@ -1,35 +1,18 @@
 import { GetBunnyModel } from "./models/stanfordbunny.js";
 import { WebGlHost } from "./webglhost.js";
+import { CodeParcel } from "./code_parcel.js";
 export class LightingDemo {
-    constructor() {
-        this.vertexShaderCode = 'attribute vec3 a_position;\r\n' +
-            'attribute vec3 a_normal;\r\n\r\n' +
-            'uniform float u_rotation;\r\n\r\n' +
-            'varying vec3 v_normal;\r\n\r\n' +
-            'void main(void) {\r\n' +
-            'float c = cos(u_rotation);' +
-            'float s = sin(u_rotation);' +
-            'mat4 rot = mat4 (c,0,-s,0, 0,1,0,0, s,0,c,0, 0,0,0,1);' +
-            ' gl_Position = rot * vec4(a_position, 1.0);\r\n' +
-            ' v_normal = a_normal;\r\n' +
-            '}';
-        this.fragmentShaderCode = 'precision mediump float;\r\n\r\n' +
-            'varying vec3 v_normal;\r\n\r\n' +
-            'void main(void) {\r\n' +
-            ' gl_FragColor = vec4(v_normal * 0.5 + 0.5, 1.0);\r\n' +
-            '}';
-        this.model = GetBunnyModel();
-        this.startingCode = this.fragmentShaderCode;
-        this.initializeDemo(this.model);
-        this.populateTextArea();
+    constructor(model, parcel) {
+        this.initializeDemo(model, parcel.vertexShaderCode, parcel.fragmentShaderCode);
+        this.populateTextArea(parcel.startingCode);
     }
-    populateTextArea() {
+    populateTextArea(startingCode) {
         const codeSection = document.getElementById("code");
         if (codeSection instanceof HTMLTextAreaElement) {
-            codeSection.value = this.startingCode;
+            codeSection.value = startingCode;
         }
     }
-    initializeDemo(model) {
+    initializeDemo(model, vertexShaderCode, fragmentShaderCode) {
         const canvas = document.getElementById("webGLCanvas");
         if (canvas instanceof HTMLCanvasElement) {
             const gl = canvas.getContext("webgl");
@@ -38,11 +21,29 @@ export class LightingDemo {
                 gl.enable(gl.DEPTH_TEST);
                 gl.clear(gl.COLOR_BUFFER_BIT);
                 gl.viewport(0, 0, canvas.width, canvas.height);
-                this.host = new WebGlHost(gl, model.vertices, model.indices, this.vertexShaderCode, this.fragmentShaderCode, "lighting");
+                this.host = new WebGlHost(gl, model.vertices, model.indices, vertexShaderCode, fragmentShaderCode, "lighting");
                 this.host.bindPositionAndNormal();
                 this.host.startRotationLoop();
             }
         }
     }
 }
-new LightingDemo();
+const vertexShaderCode = 'attribute vec3 a_position;\r\n' +
+    'attribute vec3 a_normal;\r\n\r\n' +
+    'uniform float u_rotation;\r\n\r\n' +
+    'varying vec3 v_normal;\r\n\r\n' +
+    'void main(void) {\r\n' +
+    'float c = cos(u_rotation);' +
+    'float s = sin(u_rotation);' +
+    'mat4 rot = mat4 (c,0,-s,0, 0,1,0,0, s,0,c,0, 0,0,0,1);' +
+    ' gl_Position = rot * vec4(a_position, 1.0);\r\n' +
+    ' v_normal = a_normal;\r\n' +
+    '}';
+const fragmentShaderCode = 'precision mediump float;\r\n\r\n' +
+    'varying vec3 v_normal;\r\n\r\n' +
+    'void main(void) {\r\n' +
+    ' gl_FragColor = vec4(v_normal * 0.5 + 0.5, 1.0);\r\n' +
+    '}';
+const bunnyModel = GetBunnyModel();
+const parcel = new CodeParcel(vertexShaderCode, fragmentShaderCode, fragmentShaderCode);
+new LightingDemo(bunnyModel, parcel);
--- src/script_lighting.ts
+++ src/script_lighting.ts
@@ -2,50 +2,27 @@
 import { Model } from "./model.js";
 import { GetBunnyModel } from "./models/stanfordbunny.js";
 import { WebGlHost } from "./webglhost.js";
+import { CodeParcel } from "./code_parcel.js";
 
 export class LightingDemo {
 
-    public vertexShaderCode: string =
-        'attribute vec3 a_position;\r\n' +
-        'attribute vec3 a_normal;\r\n\r\n' +
-        'uniform float u_rotation;\r\n\r\n' +
-        'varying vec3 v_normal;\r\n\r\n' +
-        'void main(void) {\r\n' +
-        'float c = cos(u_rotation);' +
-        'float s = sin(u_rotation);' +
-        'mat4 rot = mat4 (c,0,-s,0, 0,1,0,0, s,0,c,0, 0,0,0,1);' +
-        ' gl_Position = rot * vec4(a_position, 1.0);\r\n' +
-        ' v_normal = a_normal;\r\n' +
-        '}';
-
-    public fragmentShaderCode: string =
-        'precision mediump float;\r\n\r\n' +
-        'varying vec3 v_normal;\r\n\r\n' +
-        'void main(void) {\r\n' +
-        ' gl_FragColor = vec4(v_normal * 0.5 + 0.5, 1.0);\r\n' +
-        '}';
-
-    public startingCode: string;
-
-    public model: Model = GetBunnyModel();
     public host: WebGlHost | undefined;
 
-    constructor() {
+    constructor(model: Model, parcel: CodeParcel) {
 
-        this.startingCode = this.fragmentShaderCode;
-        this.initializeDemo(this.model);
-        this.populateTextArea();
+        this.initializeDemo(model, parcel.vertexShaderCode, parcel.fragmentShaderCode);
+        this.populateTextArea(parcel.startingCode);
     }
 
-    public populateTextArea() {
+    public populateTextArea(startingCode: string) {
 
         const codeSection: HTMLElement | null = document.getElementById("code");
         if (codeSection instanceof HTMLTextAreaElement) {
-            codeSection.value = this.startingCode;
+            codeSection.value = startingCode;
         }
     }
 
-    public initializeDemo(model: Model) {
+    public initializeDemo(model: Model, vertexShaderCode: string, fragmentShaderCode: string) {
 
         const canvas: HTMLElement | null = document.getElementById("webGLCanvas");
 
@@ -60,7 +37,7 @@ export class LightingDemo {
                 gl.clear(gl.COLOR_BUFFER_BIT);
                 gl.viewport(0, 0, canvas.width, canvas.height);
 
-                this.host = new WebGlHost(gl, model.vertices, model.indices, this.vertexShaderCode, this.fragmentShaderCode, "lighting");
+                this.host = new WebGlHost(gl, model.vertices, model.indices, vertexShaderCode, fragmentShaderCode, "lighting");
                 this.host.bindPositionAndNormal();
                 this.host.startRotationLoop();
             }
@@ -68,5 +45,28 @@ export class LightingDemo {
     }
 }
 
-new LightingDemo();
+const vertexShaderCode: string =
+    'attribute vec3 a_position;\r\n' +
+    'attribute vec3 a_normal;\r\n\r\n' +
+    'uniform float u_rotation;\r\n\r\n' +
+    'varying vec3 v_normal;\r\n\r\n' +
+    'void main(void) {\r\n' +
+    'float c = cos(u_rotation);' +
+    'float s = sin(u_rotation);' +
+    'mat4 rot = mat4 (c,0,-s,0, 0,1,0,0, s,0,c,0, 0,0,0,1);' +
+    ' gl_Position = rot * vec4(a_position, 1.0);\r\n' +
+    ' v_normal = a_normal;\r\n' +
+    '}';
+
+const fragmentShaderCode: string =
+    'precision mediump float;\r\n\r\n' +
+    'varying vec3 v_normal;\r\n\r\n' +
+    'void main(void) {\r\n' +
+    ' gl_FragColor = vec4(v_normal * 0.5 + 0.5, 1.0);\r\n' +
+    '}';
+
+    
+const bunnyModel: Model = GetBunnyModel();
+const parcel = new CodeParcel(vertexShaderCode, fragmentShaderCode, fragmentShaderCode);
+new LightingDemo(bunnyModel, parcel);
 
