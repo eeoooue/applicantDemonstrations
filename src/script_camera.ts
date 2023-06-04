3ce1a897873c706050913fe014a672330261b594
eeoooue <pdmoorhouse@gmail.com>
2023-06-04 10:15:26 +0100

    camera page uses CodeParcel

--- src/script_camera.ts
+++ src/script_camera.ts
@@ -1,47 +1,26 @@
 
+import { CodeParcel } from "./code_parcel.js";
 import { Model } from "./model.js";
 import { GetSphereModel } from "./models/sphere.js";
 import { WebGlHost } from "./webglhost.js";
 
 export class CameraDemo {
 
-    public vertexShaderCode: string =
-        'attribute vec3 a_position;\r\n' +
-        'attribute vec3 a_normal;\r\n\r\n' +
-        'uniform vec3 u_cameraPosition;\r\n\r\n' +
-        'varying vec4 v_colour;\r\n\r\n' +
-        'void main(void) {\r\n' +
-        ' gl_Position = vec4(a_position, 1.0);\r\n' +
-        ' v_colour = vec4(a_normal * 0.5 + 0.5, 1.0);\r\n' +
-        '}';
+    constructor(model: Model, parcel: CodeParcel) {
 
-    public fragmentShaderCode: string =
-        'precision mediump float;' +
-        'varying vec4 v_colour;' +
-        'void main(void) {' +
-        ' gl_FragColor = v_colour;' +
-        '}';
-
-    public startingCode: string;
-
-    public model: Model = GetSphereModel();
-
-    constructor() {
-
-        this.startingCode = this.vertexShaderCode;
-        this.initializeDemo(this.model);
-        this.populateTextArea();
+        this.initializeDemo(model, parcel.vertexShaderCode, parcel.fragmentShaderCode, "camera");
+        this.populateTextArea(parcel.startingCode);
     }
 
-    public populateTextArea() {
+    public populateTextArea(startingCode: string) {
 
         const codeSection: HTMLElement | null = document.getElementById("code");
         if (codeSection instanceof HTMLTextAreaElement) {
-            codeSection.value = this.startingCode;
+            codeSection.value = startingCode;
         }
     }
 
-    public initializeDemo(model: Model) {
+    public initializeDemo(model: Model, vertexShaderCode: string, fragmentShaderCode: string, pageTitle: string) {
 
         const canvas: HTMLElement | null = document.getElementById("webGLCanvas");
 
@@ -56,7 +35,7 @@ export class CameraDemo {
                 gl.clear(gl.COLOR_BUFFER_BIT);
                 gl.viewport(0, 0, canvas.width, canvas.height);
 
-                var host = new WebGlHost(gl, model.vertices, model.indices, this.vertexShaderCode, this.fragmentShaderCode, "camera");
+                var host = new WebGlHost(gl, model.vertices, model.indices, vertexShaderCode, fragmentShaderCode, pageTitle);
                 host.bindPositionAndNormal();
                 host.setupCameraMovement();
             }
@@ -64,4 +43,26 @@ export class CameraDemo {
     }
 }
 
-new CameraDemo();
+
+const vertexShaderCode: string =
+    'attribute vec3 a_position;\r\n' +
+    'attribute vec3 a_normal;\r\n\r\n' +
+    'uniform vec3 u_cameraPosition;\r\n\r\n' +
+    'varying vec4 v_colour;\r\n\r\n' +
+    'void main(void) {\r\n' +
+    ' gl_Position = vec4(a_position, 1.0);\r\n' +
+    ' v_colour = vec4(a_normal * 0.5 + 0.5, 1.0);\r\n' +
+    '}';
+
+const fragmentShaderCode: string =
+    'precision mediump float;' +
+    'varying vec4 v_colour;' +
+    'void main(void) {' +
+    ' gl_FragColor = v_colour;' +
+    '}';
+
+const startingCode: string = vertexShaderCode;
+
+const parcel = new CodeParcel(vertexShaderCode, fragmentShaderCode, startingCode);
+const sphereModel: Model = GetSphereModel();
+new CameraDemo(sphereModel, parcel);
